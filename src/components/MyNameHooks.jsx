19ca981941a7066237863282977be5ae493a35d5
Mariusz <urban.mariusz@icloud.com>
2021-01-31 21:18:32 +0100

    done

---
+++ src/components/MyNameHooks.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default function MyNameHooks() {
+  const [name, setName] = useState("");
+  const change = (e) => {
+    setName(e.target.value);
+  };
+  return (
+    <div>
+      <h1>My Hook Name is : {name}</h1>
+      <input type="text" value={name} onChange={change} />
+    </div>
+  );
+}

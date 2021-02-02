b4ce74ecbaf02eaaa94aff8b2154eb0757133a0b
Mariusz <urban.mariusz@icloud.com>
2021-02-02 18:16:23 +0100

    conditional hooks done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function useState(initialState) {
   let hookIndex = currentHook;
   const setState = (nextValue) => {
     values[hookIndex] = nextValue;
+    console.log("🚀 ~ setState ~ values", values);
     ReactDOM.render(<MyNameHooks />, document.getElementById("root"));
   };
   return [values[currentHook++], setState];
@@ -15,23 +16,35 @@ function useState(initialState) {
 
 function MyNameHooks() {
   currentHook = 0;
+  const [enableFirstName, setEnableFirstName] = useState(false);
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
 
-  const changeName = (e) => {
+  const handleChangeName = (e) => {
     setName(e.target.value);
   };
 
-  const changeLastName = (e) => {
+  const handleChangeLastName = (e) => {
     setLastName(e.target.value);
   };
+
+  const handleEnableChange = (e) => {
+    setEnableFirstName(!enableFirstName);
+  };
+
   return (
     <div>
       <h1>
-        My Hook Name is : {name} and my last name is: {lastName}
+        My Hook Name is : {enableFirstName ? name : ""} and my last name is:{" "}
+        {lastName}
       </h1>
-      <input id="name" type="text" value={name} onChange={changeName} />
-      <input type="text" value={lastName} onChange={changeLastName} />
+      <input
+        type="checkbox"
+        value={enableFirstName}
+        onChange={handleEnableChange}
+      />
+      <input id="name" type="text" value={name} onChange={handleChangeName} />
+      <input type="text" value={lastName} onChange={handleChangeLastName} />
     </div>
   );
 }

f7132ffae8c8418872115a5ec0c5b999175d7c42
Mariusz <urban.mariusz@icloud.com>
2021-02-02 16:43:51 +0100

    multipleState done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-let value;
+let values = [];
+let currentHook = 0;
+
 function useState(initialState) {
-  if (typeof value === "undefined") value = initialState;
+  if (typeof values === "undefined") values[currentHook] = initialState;
+  let hookIndex = currentHook;
   const setState = (nextValue) => {
-    value = nextValue;
+    values[hookIndex] = nextValue;
     ReactDOM.render(<MyNameHooks />, document.getElementById("root"));
   };
-  return [value, setState];
+  return [values[currentHook++], setState];
 }
 
 function MyNameHooks() {
+  currentHook = 0;
   const [name, setName] = useState("");
-  const change = (e) => {
+  const [lastName, setLastName] = useState("");
+
+  const changeName = (e) => {
     setName(e.target.value);
+  };
+
+  const changeLastName = (e) => {
+    setLastName(e.target.value);
   };
   return (
     <div>
-      <h1>My Hook Name is : {name}</h1>
-      <input type="text" value={name} onChange={change} />
+      <h1>
+        My Hook Name is : {name} and my last name is: {lastName}
+      </h1>
+      <input id="name" type="text" value={name} onChange={changeName} />
+      <input type="text" value={lastName} onChange={changeLastName} />
     </div>
   );
 }

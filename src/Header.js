63c916aa6ae8b8ab5483eea9e1977edc94970712
Mariusz <urban.mariusz@icloud.com>
2021-02-04 22:25:57 +0100

    done

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "./context";
+import React, { useContext } from 'react'
+import { ThemeContext } from './contexts'
 
-export default function Header({ text }) {
-  const { primaryColor } = useContext(ThemeContext);
-
-  return <h1 style={{ color: primaryColor }}>{text}</h1>;
+const Header = ({ text }) => {
+  const { primaryColor } = useContext(ThemeContext)
+  return <h1 style={{ color: primaryColor }}>{text}</h1>
 }
+
+export default Header
